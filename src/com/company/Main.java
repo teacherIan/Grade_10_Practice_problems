bc04b73bed6f0f303c67d8e85c15fdb9aee56316
Ian Malloy <mellomalloy@gmail.com>
2021-12-03 12:23:53 +0800

    For Scott

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,14 +12,53 @@ public class Main {
          *
          */
 
+        //print all numbers between 1 - 100
+
+        //print only even numbers between 1 - 100
+
+        //print all odd numbers between 1 - 100
+
+        /*
+         * Create functions named "A", "B", "C", "D", and "E"
+         * "A","B","C","D" should take in two Integer variables(parameters) and return an integer
+         * "A" should return the sum of the two parameters
+         * "B" should return the difference of the two parameters
+         * "C" should return the product of the two parameters
+         * "D" should return the quotient of the two parameters
+         * "E" should have one integer parameter, and not return anything
+         * "E" should print out the parameter along with a useful message
+         *
+         * Demo that you built functions A,B,C,D,E correctly by calling them in the main method with different inputs
+         *
+         */
+
+
+
+        /*
+         * Create a function called fizzBuzz which takes in one Integer parameter and does not return anything
+         *
+         * Console out a welcome message to the user and tell them the rules of fizzBuzz.
+         * Ask the user to pick a maximum number
+         *
+         *
+         *                  FIZZBUZZ RULES
+         * "Write a program that prints the numbers from 1 to user input
+         *  But for multiples of three print “Fizz” instead of the number
+         *  and for the multiples of five print “Buzz”.
+         * For numbers which are multiples of both three and five print “FizzBuzz”."
+         *
+         *
+         * Call the fizzbuzz function with some different input to check if it is working.
+         *
+         *
+         */
+
 
     }
 
-    //print all numbers between 1 - 100
 
-    //print all even numbers
 
-    //print all odd numbers
+
 
 
 }
