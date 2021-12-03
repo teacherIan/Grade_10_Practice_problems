c690b94651a2c3837dc1784a0a806c65345337b3
Ian Malloy <mellomalloy@gmail.com>
2021-12-03 08:25:36 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+         * Practice problems.  Problems can have multiple correct implementations,
+         * and can be solved in multiple ways.
+         * functions can be used on all problems, if so desired.
+         *
+         *
+         */
+
+
+    }
+
+    //print all numbers between 1 - 100
+
+    //print all even numbers
+
+    //print all odd numbers
+
+
+}
